2fc075e00435d681c1e7e74289c9fb449313076a
GjergjiSh <gjergji.shkurti@yahoo.com>
2025-05-10 15:24:20 +0200

    initial commit

---
+++ tests/a2lwriter_tests.cpp
@@ -0,0 +1,10 @@
+#include "a2lwriter.h"
+#include <gtest/gtest.h>
+
+class x_prjctTests : public ::testing::Test {
+protected:
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(x_prjctTests, ExampleTest) { EXPECT_TRUE(true); }
